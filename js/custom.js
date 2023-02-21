76cb5aae88b6287bde788d9b87cb308832d86d6a
libertysea <1378600591@qq.com>
2023-02-21 23:04:48 +0800

    Site updated: 2023-02-21 23:04:48

--- js/custom.js
+++ js/custom.js
@@ -10,10 +10,23 @@ if (document.title === title){
 
 
 
-var $percent = document.querySelector("#nav #hotkey #top-button a.site-page i");
-$percent && window.addEventListener("scroll", (function () {
-    let e = document.body.scrollHeight || document.documentElement.scrollHeight,
-        t = window.innerHeight || document.documentElement.clientHeight;
-    $percent.dataset.percent = ((document.body.scrollTop || document.documentElement.scrollTop) / (e - t) * 100).toFixed(0)
-}));
+// var $percent = document.querySelector("#nav #hotkey #top-button a.site-page a.totopbtn>span").innerHTML;
+// $percent && window.addEventListener("scroll", (function () {
+//     let e = document.body.scrollHeight || document.documentElement.scrollHeight,
+//         t = window.innerHeight || document.documentElement.clientHeight;
+//     $percent.dataset.percent = ((document.body.scrollTop || document.documentElement.scrollTop) / (e - t) * 100).toFixed(0)
+// }));
+
+window.onscroll = percent;// 执行函数
+// 页面百分比
+function percent() {
+    let a = document.documentElement.scrollTop || window.pageYOffset, // 卷去高度
+        b = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.body.clientHeight, document.documentElement.clientHeight) - document.documentElement.clientHeight, // 整个网页高度
+        result = Math.round(a / b * 100)
+
+    document.querySelector(".totopbtn>span").innerHTML = result;
+
+}
+
+
 

da0926f5090b15750559481b6e8d545d3bbd2b60
libertysea <1378600591@qq.com>
2023-04-16 23:15:15 +0800

    Site updated: 2023-04-16 23:15:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/04/03/CloudSearchAggregationPlatform/","2023/04/02/Typora+PicGo+Lsky Pro搭建写作环境/","2023/01/13/deeplearning/基于深度学习的中医药识别与预测/基于深度学习的中医药识别与预测/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/ed866ec2.html","posts/c03ae513.html","posts/e323552.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};

0dded065c07c573c4e178280bc010158c8a4cae3
libertysea <1378600591@qq.com>
2023-05-02 18:51:11 +0800

    Site updated: 2023-05-02 18:51:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/c03ae513.html","posts/ed866ec2.html","posts/9f5c66d2.html","posts/3cbdce34.html","posts/629787b4.html","posts/e323552.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/9f5c66d2.html","posts/ed866ec2.html","posts/c03ae513.html","posts/3cbdce34.html","posts/e323552.html","posts/629787b4.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};

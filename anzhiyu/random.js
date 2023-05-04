30c975de22bbc76e5a97ae9c020956219035609f
libertysea <1378600591@qq.com>
2023-05-04 09:57:10 +0800

    Site updated: 2023-05-04 09:57:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/ed866ec2.html","posts/629787b4.html","posts/9f5c66d2.html","posts/3cbdce34.html","posts/e323552.html","posts/c03ae513.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/ed866ec2.html","posts/c03ae513.html","posts/undefined.html","posts/9f5c66d2.html","posts/629787b4.html","posts/3cbdce34.html","posts/e323552.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
